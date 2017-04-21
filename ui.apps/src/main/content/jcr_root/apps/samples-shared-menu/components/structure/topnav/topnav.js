f56ad93d50304a71d0621a59d18e699a72d52600
Daniel Henrique Alves Lima <daniel@danielhalima.com>
2017-04-21 13:32:26 -0300

    Turning topnav into a configurable component

--- ui.apps/src/main/content/jcr_root/apps/samples-shared-menu/components/structure/topnav/topnav.js
+++ ui.apps/src/main/content/jcr_root/apps/samples-shared-menu/components/structure/topnav/topnav.js
@@ -2,21 +2,28 @@
 // Server-side JavaScript for the topnav logic
 use(function () {
     var items = [];
-    var root = currentPage.getAbsoluteParent(1);
-    var currentNavPath = currentPage.getAbsoluteParent(2).getPath();
-    var it = root.listChildren(new Packages.com.day.cq.wcm.api.PageFilter());
+	
+	// get starting point of navigation
+    var absParent = properties.get('absParent', currentPage.getParent().getPath());
+    log.debug('absParent = {}', absParent);
+    
+    var root = currentPage.getPageManager().getPage(absParent);
+    if (root) {
+    	var currentNavPath = currentPage.getAbsoluteParent(2).getPath();
+    	var it = root.listChildren(new Packages.com.day.cq.wcm.api.PageFilter());
 
-    while (it.hasNext()) {
-        var page = it.next();
+    	while (it.hasNext()) {
+    		var page = it.next();
 
-        // No strict comparison, because the types returned from the Java APIs
-        // don't strictly match the JavaScript types
-        var selected = (page.getPath() == currentNavPath);
+    		// No strict comparison, because the types returned from the Java APIs
+    		// don't strictly match the JavaScript types
+    		var selected = (page.getPath() == currentNavPath);
 
-        items.push({
-            page: page,
-            selected : selected
-        });
+    		items.push({
+    			page: page,
+    			selected : selected
+    		});
+    	}
     }
 
     return {
